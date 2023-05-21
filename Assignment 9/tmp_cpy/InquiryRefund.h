89d2db01f2ab7fb26e7287a76f95d5c5bf584b76
KXAINGW <94122424+KXAINGW@users.noreply.github.com>
2023-05-22 00:07:18 +0800

    Add files via upload

---
+++ Assignment 9/tmp_cpy/InquiryRefund.h
@@ -0,0 +1,32 @@
+#ifndef InquiryRefund_H
+#define InquiryRefund_H
+#ifndef maxNumReservations
+#define maxNumReservations 1000
+#endif // !maxNumReservations
+
+#include "ReservationDatabase.h"
+#include "FlightSchedule.h"
+
+extern const char airportName[12][12];
+extern const int fullFare[12][12];
+extern const int discount[8];
+
+
+class InquiryRefund
+{
+public:
+	InquiryRefund(ReservationDatabase& res, FlightSchedule& fs);
+	void bookingInquiry(string id);
+	void refundApplication(string id);
+
+private:
+	ReservationDatabase& reservations;
+	FlightSchedule& allflightSchedule;
+	bool exist;
+	//member function
+	void loadReservations(ReservationDatabase reservations[], int& numReservations);
+	void loadFlightSchedules(FlightSchedule& flightSchedule);
+	void displayReservation(ReservationDatabase& reservation, const FlightSchedule& flightSchedule);
+};
+
+#endif
---
+++ Assignment 9/tmp_cpy/MakingReservation.h
@@ -0,0 +1,39 @@
+#ifndef MakingReservation_H
+#define MakingReservation_H
+#include "ReservationDatabase.h"
+#include "FlightSchedule.h"
+#include<iostream>
+
+using std::cout;
+using std::cin;
+using std::cerr;
+using std::endl;
+
+// It cannot use external in cpp file
+extern const char airportName[12][12];
+extern const int fullFare[12][12];
+extern const int discount[8];
+
+const int maxNumReservations = 1000;
+class MakingReservation
+{
+public:
+	MakingReservation(ReservationDatabase& res, FlightSchedule& fs);
+	void execute(); 
+private:
+	ReservationDatabase& reservation;
+	FlightSchedule& flightSchedule;
+
+	// function member
+	void chooseArrivalAirport(int departureAirport, int& arrivalAirport);
+	void loadFlightSchedules(FlightSchedule& flightSchedule, int departureAirport, int arrivalAirport);
+	void chooseFlight(ReservationDatabase& reservation, const FlightSchedule& flightSchedule);
+	void inputNumTickets(ReservationDatabase& reservation);
+	void displayReservation(ReservationDatabase& reservation, const FlightSchedule& flightSchedule);
+	bool duplicate(ReservationDatabase& reservation);
+	void loadReservations(ReservationDatabase reservations[], int& numReservations);
+	void saveReservation(ReservationDatabase& reservation);
+
+};
+
+#endif
