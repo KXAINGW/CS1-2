03f683d1af2b24fcc96edd6c272a00f5364aa025
KXAINGW <94122424+KXAINGW@users.noreply.github.com>
2023-05-22 00:10:28 +0800

    Add files via upload

---
+++ Assignment 9/tmp_cpy/FlightSchedule.cpp
@@ -0,0 +1,24 @@
+#include "FlightSchedule.h"
+
+FlightSchedule::FlightSchedule() :flights() {}
+
+vector<FlightDatabase> FlightSchedule::getFlightDatabase() const
+{
+	return flights;
+}
+
+vector<FlightDatabase>& FlightSchedule::getFlightDatabase_ref()
+{
+	return flights;
+}
+
+int FlightSchedule::getNumFlights() const
+{
+	return flights.size();
+}
+
+void FlightSchedule::add(const FlightDatabase& flight)
+{
+	flights.push_back(flight);
+}
+
